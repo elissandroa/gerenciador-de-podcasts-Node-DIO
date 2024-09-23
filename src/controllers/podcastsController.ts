9dcb6c3602761a65ad3df91570038c64b05cedef
elissandroa <elissandroa@hotmail.com>
2024-09-23 16:15:15 -0300

    Refatorando-Camadas

--- src/controllers/podcastsController.ts
+++ src/controllers/podcastsController.ts
@@ -3,18 +3,14 @@ import { serviceListEpisodes } from '../services/listEpisodesService';
 import { serviceFilterEpisodes } from '../services/filterEpisodesService';
 
 
-
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
     res.writeHead(200, { 'Content-Type': 'application/json' })
     const content = await serviceListEpisodes()
     res.end(content)
-
 }
 
 export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
     res.writeHead(200, { 'Content-Type': 'application/json' })
-    let queryString = req.url?.split('?p=')[1] ?? ""
-    
-    const content = await serviceFilterEpisodes(queryString)
+    const content = await serviceFilterEpisodes(req.url)
     res.end(content);
 }
--- src/services/filterEpisodesService.ts
+++ src/services/filterEpisodesService.ts
@@ -1,7 +1,14 @@
+import { IncomingMessage } from "http";
 import { repositoryPodcast } from "../repositories/podcastsRepository";
 
-export const serviceFilterEpisodes = async (podcastName:string) => {
-    const data = await repositoryPodcast(podcastName);
+export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
+
+
+    let queryString = podcastName?.split('?p=')[1] ?? ""
+
+    const data = await repositoryPodcast(queryString);
+
+
 
     return JSON.stringify(data);
 }
