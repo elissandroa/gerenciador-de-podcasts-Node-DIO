51440d34757a410a187260715c7c26549062ebc9
elissandroa <elissandroa@hotmail.com>
2024-09-23 11:33:43 -0300

    Camadas iniciais da Aplicação / Server Http

--- dist/server.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-// src/server.ts
-console.log("Ol\xE1 Node.JS");
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("Olá Node.JS");
+import * as http from 'http';
+
+const port = process.env.PORT;
+
+const server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {
+})
+
+server.listen(port, () => {
+    console.log(`Rodando na porta ${port}`);
+})
+
+
+
