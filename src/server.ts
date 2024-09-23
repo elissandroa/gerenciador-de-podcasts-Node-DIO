611495e64f9135324bbc3f8fd0399df11d5d622c
elissandroa <elissandroa@hotmail.com>
2024-09-23 12:27:35 -0300

    Criando o Controller

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import * as http from 'http';
+import { getListEpisodes } from './controllers/podcastsController';
 
 const port = process.env.PORT;
 
 const server = http.createServer(
-    (request: http.IncomingMessage, response: http.ServerResponse) => {
-})
+   async (req: http.IncomingMessage, res: http.ServerResponse) => {
+        if(req.method === "GET"){
+           await getListEpisodes(req, res);
+        }
+    })
 
 server.listen(port, () => {
     console.log(`Rodando na porta ${port}`);
